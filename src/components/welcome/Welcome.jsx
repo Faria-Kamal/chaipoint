817e28fd179612cbcbece5f432443f2ab4094406
zer0-byte0 <k.fariasoha@gmail.com>
2022-07-24 15:40:24 +0600

    welcome updated

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Welcome.css";
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h2 className="title">Brightening Lives & Bringing People Together</h2>
+      <h5 className="para">
+        {" "}
+        With over 30 Lakh happy customers around the country and 10+ Years of
+        expertise in
+      </h5>
+      <h5 className="para">
+        brewing a great cup for the perfect experience, Chai Point brings the
+        same expertise and
+      </h5>
+
+      <h5 className="para">same quality, one step closer</h5>
+      <h5 className="para">
+        Now enjoy the Chai Point Experience at the comfort of your homes, as we
+        curate
+      </h5>
+      <h5 className="para">specialized blends personalized to your taste. </h5>
+      <h5 className="para1">
+        {" "}
+        Welcome to India's first Home Cafe by Chai Point!
+      </h5>
+    </div>
+  );
+}
+
+export default Welcome;
