bb953fbcdf5f44bfe660cdf63d4d231996bf4d2d
zer0-byte0 <k.fariasoha@gmail.com>
2022-07-24 15:38:59 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Banner from "./components/banner/Banner";
+import Welcome from "./components/welcome/Welcome";
+import BestSellers from "./components/best sellers/BestSellers";
+import Gift from "./components/gift/Gift";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Banner />
+      <Welcome />
+      <BestSellers />
+      <Gift />
     </div>
   );
-}
+};
 
 export default App;
